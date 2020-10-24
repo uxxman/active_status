a21f336aa2bf0f3aef9016b9641236b29b5551f0
Muhammad Usman <uxman.sherwani@gmail.com>
2020-10-25 01:13:35 +0200

    Setting up rspec for testing

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,19 +1,9 @@
 require_relative 'boot'
 
-require "rails"
-# Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require "rails/all"
 
 Bundler.require(*Rails.groups)
+
 require "rails_health_status"
 
 module Dummy
@@ -27,4 +17,3 @@ module Dummy
     # the framework and any gems in your application.
   end
 end
-
---
+++ spec/rails_helper.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+ENV['RAILS_ENV']  ||= 'test'
+ENV['RAILS_ROOT'] ||= File.dirname(__FILE__) + '../../../spec/dummy'
+
+require File.expand_path('../spec/dummy/config/environment.rb', __dir__)
+
+abort('The Rails environment is running in production mode!') if Rails.env.production?
+
+require 'rspec/rails'
+
+RSpec.configure do |config|
+  # The different available types are documented in the features, such as in
+  # https://relishapp.com/rspec/rspec-rails/docs
+  config.infer_spec_type_from_file_location!
+
+  # Filter lines from Rails gems in backtraces.
+  config.filter_rails_from_backtrace!
+  # arbitrary gems may also be filtered via:
+  # config.filter_gems_from_backtrace("gem name")
+end
